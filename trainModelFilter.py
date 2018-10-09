fc090310c78db21e1318a409183f8833b5b8497c
rihk <rihk@kth.se>
2018-10-09 18:07:24 +0200

    added few stuffs

--- trainModelFilter.py
+++ trainModelFilter.py
@@ -23,3 +23,34 @@ def getSentences(directory):
 
 get = getSentences("extracted")
 get.next()
+
+
+#input- single json file and titles not required
+#output - list of list of documents excluding not required titles
+
+import json
+from os import walk
+#for a single file
+def json_readr(file):
+    neg_titles=["Chromosome", "Charles Martel", "Celestines"]
+    l = []    
+    for line in open(file, mode="r",encoding="utf-8"):
+        li = []
+        a = json.loads(line).get("title")
+        if a not in neg_titles:
+            li = json.loads(line).get("text")        
+            l.append(li)
+    return l 
+
+#for all files    
+path = "E:\extracted"
+lis = []
+for (dirpath, dirnames,filenames) in walk(path):
+    for fi in filenames: 
+        lst = json_readr(os.path.join(dirpath,fi))
+        lis.extend(lst)
+
+#write the extracted sentences to file        
+with open('sent_to_train.txt', 'w') as f:
+    for item in lis:
+        f.write("%s\n" % item)

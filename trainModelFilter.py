26c12981f63e20377d28d3970471b596e1f36ce5
rihk <rihk@kth.se>
2018-10-10 13:01:52 +0200

    trainer

--- trainModelFilter.py
+++ trainModelFilter.py
@@ -24,39 +24,49 @@ def getSentences(directory):
 #get = getSentences("extracted")
 #get.next()
 
-#input- single json file and titles not required
-#output - list of list of documents excluding not required titles
-
 import json
 import os
 import re
+import sys
 from os import walk
-#for a single file
-def json_readr(file):
-    neg_titles=["Chromosome", "Charles Martel", "Celestines"]
-    l = []    
-    for line in open(file, mode="r",encoding="utf-8"):
-        li = []
-        a = json.loads(line).get("title")
-        if a not in neg_titles:
-            li = []
-            li = json.loads(line).get("text") 
-            # lit = re.sub('\][-*&^%$#@!?><,.',' ',li)
-            # lite = [w.lower() for w in lit.spilt()]
-            z = re.sub(r'[™!®#\'()*+,-./:;<=>?@\&[\]^_`{|}~"’”“′‘\\\\%0123456789£'',\n()]'," ",li)       
-            w = [w.lower() for w in str(z).split()]    
-            l.append(w)              
+import multiprocessing
+from gensim.models import word2vec
+
+#generates list of list of words.
+def json_readr(path):
+    neg_titles=["Dance", "History_of_dance", "List_of_dance_style_categories", "List_of_national_dances", "List_of_ethnic,_regional,_and_folk_dances_by_origin", "List_of_dance_occupations","Choreography","Outline_of_dance","Dance_move","Dance_technology","List_of_dance_companies","List_of_dancers","Group_dance","Dance_and_health","Dancing_mania"
+               ,"Modern_dance","Dance_education","Dance_in_mythology_and_religion","Dance_therapy","Dance_music"] 
+    l = []
+    for (dirpath, dirnames,filenames) in walk(path):
+        for fi in filenames:
+            for line in open(os.path.join(dirpath,fi), mode="r",encoding="utf-8"):
+                li = []
+                a = json.loads(line).get("title")
+                if a not in neg_titles:
+                    li = []
+                    li = json.loads(line).get("text") 
+                    z = re.sub(r'[™!®#\'()*+,-./:;<=>?@\&[\]^_`{|}~"’”“′‘\\\\%0123456789£'',\n()]'," ",li)       
+                    w = [w.lower() for w in str(z).split()]    
+                    l.append(w)              
     yield l 
 
-#for all files    
-path = "E:\extracted"
-lis = []
-for (dirpath, dirnames,filenames) in walk(path):
-    for fi in filenames: 
-        lst = json_readr(os.path.join(dirpath,fi))
-        print(lst)
-        lis.extend(lst)
+#train word2vec
+if __name__ == '__main__':
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+ 
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+  
+    path = "E:\extracted"    
+    model = Word2Vec(json_readr(path), size=400, 
+        window=5, min_count=5, workers=multiprocessing.cpu_count())
+    # trim unneeded model memory = use (much) less RAM
+    model.init_sims(replace=True)
+    model.save("novice_word_vec_wiki.model")
+
+# with open('sent_to_train.txt', 'w') as f:
+    # for item in lis:
+        # f.write("%s\n" % item)
     
-with open('sent_to_train.txt', 'w') as f:
-    for item in lis:
-        f.write("%s\n" % item)

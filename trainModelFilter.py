a914a423701045b261a574476a9c689e2e6d7ff5
rihk <rihk@kth.se>
2018-10-10 11:40:36 +0200

    re

--- trainModelFilter.py
+++ trainModelFilter.py
@@ -24,11 +24,12 @@ def getSentences(directory):
 #get = getSentences("extracted")
 #get.next()
 
-
 #input- single json file and titles not required
 #output - list of list of documents excluding not required titles
 
 import json
+import os
+import re
 from os import walk
 #for a single file
 def json_readr(file):
@@ -38,9 +39,14 @@ def json_readr(file):
         li = []
         a = json.loads(line).get("title")
         if a not in neg_titles:
-            li = json.loads(line).get("text")        
-            l.append(li)
-    return l 
+            li = []
+            li = json.loads(line).get("text") 
+            # lit = re.sub('\][-*&^%$#@!?><,.',' ',li)
+            # lite = [w.lower() for w in lit.spilt()]
+            z = re.sub(r'[™!®#\'()*+,-./:;<=>?@\&[\]^_`{|}~"’”“′‘\\\\%0123456789£'',\n()]'," ",li)       
+            w = [w.lower() for w in str(z).split()]    
+            l.append(w)              
+    yield l 
 
 #for all files    
 path = "E:\extracted"
@@ -48,9 +54,9 @@ lis = []
 for (dirpath, dirnames,filenames) in walk(path):
     for fi in filenames: 
         lst = json_readr(os.path.join(dirpath,fi))
+        print(lst)
         lis.extend(lst)
-
-#write the extracted sentences to file        
+    
 with open('sent_to_train.txt', 'w') as f:
     for item in lis:
         f.write("%s\n" % item)

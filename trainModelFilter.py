0b31921718b299db541bb7a7edc02c169e31b277
rihk <rihk@kth.se>
2018-10-16 14:57:57 +0200

    lessknownmodel

--- trainModelFilter.py
+++ trainModelFilter.py
@@ -1,28 +1,5 @@
-import os
-import nltk
-import json
-import gensim
-
-
-def getSentences(directory):
-		data = []
-		ll = ""
-		print("hello")
-		for subdir, dirs, files in os.walk(self.filename):
-			for fil in files:
-				print(os.path.join(subdir,fil))
-				f = open(os.path.join(subdir,fil),'r')
-				for line in f:
-					data = json.loads(line)
-					print(data)
-					ll = ll + [i for i in unicode(data, 'utf-8').lower().split()]
-			
-			print (ll)
-		yield ll
-
-
-#get = getSentences("extracted")
-#get.next()
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 
 import json
 import os
@@ -32,6 +9,12 @@ from os import walk
 import multiprocessing
 from gensim.models import word2vec
 from wikiExceptions import exceptionList
+import logging
+import multiprocessing
+#from gensim.corpora import WikiCorpus
+#from gensim.models import Word2Vec
+#from gensim.models.word2vec import LineSentence
+from time import time
 
 #generates list of list of words.
 def json_readr(path):
@@ -55,18 +38,16 @@ if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
     logger = logging.getLogger(program)
  
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
-    logging.root.setLevel(level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=logging.INFO)
     logger.info("running %s" % ' '.join(sys.argv))
   
-    path = "E:\extracted"    
+    path = "E:\\extracted"    
     model = Word2Vec(json_readr(path), size=400, 
         window=5, min_count=5, workers=multiprocessing.cpu_count())
-    # trim unneeded model memory = use (much) less RAM
-    model.init_sims(replace=True)
-    model.save("novice_word_vec_wiki.model")
+ 
+    model.save('wiki.en.novice_text.model')
+    model.wv.save_word2vec_format('wiki.en.novice_text.vector', binary=False)
+	
+end = time()
 
-# with open('sent_to_train.txt', 'w') as f:
-    # for item in lis:
-        # f.write("%s\n" % item)
-    
+print("Total procesing time: %d seconds" % (end - begin))
